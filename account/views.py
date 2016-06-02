7a1a2b24b2f55d1634cb85b749794de7a16be7d3
polar9527 <polar9527@163.com>
2016-06-03 01:52:32 +0800

    Use Django authentication views
    	use Log in and log out views of Django authentication

--- account/views.py
+++ account/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
 
 # Create your views here.
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
+
+
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
